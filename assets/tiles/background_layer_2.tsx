7e4c27c1bf0b51b1aa414771db8c3f7ed3a27495
mahesh <mahesh.mesta@codematrix.org>
2025-02-08 18:59:58 +0530

    background line bleeding fix

--- assets/tiles/background_layer_2.tsx
+++ assets/tiles/background_layer_2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="background_layer_2" tilewidth="24" tileheight="24" tilecount="91" columns="13">
+<tileset version="1.10" tiledversion="1.11.0" name="background_layer_2" tilewidth="24" tileheight="24" margin="1" tilecount="91" columns="13">
  <image source="../images/background/background_layer_2.png" width="320" height="180"/>
 </tileset>
