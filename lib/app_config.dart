edfe02e4e14517b7d37b8ef782a9f45f27d4d278
mahesh <mahesh.mesta@codematrix.org>
2026-03-08 14:34:00 +0530

    feat: Introduce `AppConfig` for application constants, update project version, and set up initial Android Gradle configuration.

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -2,7 +2,7 @@ import 'package:flutter/widgets.dart';
 
 mixin AppConfig {
   static const String appName = 'Arise Game';
-  static const String appVersion = '1.0.0';
+  static const String appVersion = '1.2.0';
   static const Size designSize = Size(913.2, 432.0);
 
   // static double widthOfView(double height) => designSize.width * height / designSize.height;
